4b594cb43a79bb0df33d7657c6ccc354e6872615
Dowen <tokenyete@gmail.com>
2019-07-08 19:52:10 +0800

    Updates

---
+++ precache-manifest.d95abc809f395966aba24c94e0956f88.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6a9b188f8c4b2a7db869e5474f24c2e",
+    "url": "/BCCSurvey/static/media/pic_action01@3x.b6a9b188.png"
+  },
+  {
+    "revision": "d69e0b5c1d4a6050cb32",
+    "url": "/BCCSurvey/static/css/main.9f118dd9.chunk.css"
+  },
+  {
+    "revision": "cea48e1e02af7282d611",
+    "url": "/BCCSurvey/static/js/runtime~main.3b6e59a7.js"
+  },
+  {
+    "revision": "a44c1d52c8ddb0126316",
+    "url": "/BCCSurvey/static/js/2.a42f0e6c.chunk.js"
+  },
+  {
+    "revision": "afa3be7ffb5b4f7eff6bcb5599cf8c1e",
+    "url": "/BCCSurvey/static/media/r.afa3be7f.png"
+  },
+  {
+    "revision": "207809fd2849a623c93591dc009f005a",
+    "url": "/BCCSurvey/static/media/i.207809fd.png"
+  },
+  {
+    "revision": "0ce33dbe203a5bedc9de981b42383425",
+    "url": "/BCCSurvey/static/media/s.0ce33dbe.png"
+  },
+  {
+    "revision": "c3b59c99dc2a79f37d32a2fd2d859c91",
+    "url": "/BCCSurvey/static/media/c.c3b59c99.png"
+  },
+  {
+    "revision": "52c748d265dd56eb05b6704e6cd2c855",
+    "url": "/BCCSurvey/static/media/a.52c748d2.png"
+  },
+  {
+    "revision": "daef4e062568e7cd76382366842e34f5",
+    "url": "/BCCSurvey/static/media/e.daef4e06.png"
+  },
+  {
+    "revision": "1bcb91142c2f4a06ea96db8098d8236f",
+    "url": "/BCCSurvey/static/media/pic_2_indroduction@3x.1bcb9114.png"
+  },
+  {
+    "revision": "d69e0b5c1d4a6050cb32",
+    "url": "/BCCSurvey/static/js/main.549b733e.chunk.js"
+  },
+  {
+    "revision": "ff74d945153e34e7a760a27ec15b7336",
+    "url": "/BCCSurvey/static/media/pic_action02@3x.ff74d945.png"
+  },
+  {
+    "revision": "cae784a733a4f2b2c0ab60cdfcf492a7",
+    "url": "/BCCSurvey/static/media/pic_action03@3x.cae784a7.png"
+  },
+  {
+    "revision": "446211f8485a501846f61a044d7ec3b8",
+    "url": "/BCCSurvey/static/media/pic_action04@3x.446211f8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BCCSurvey/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "89f7330ca872bd58fa2534642c212f68",
+    "url": "/BCCSurvey/static/media/icon_arrow.89f7330c.svg"
+  },
+  {
+    "revision": "c0a9b8535beb880d3b28c2e59f091f7e",
+    "url": "/BCCSurvey/static/media/pic_1_main page.c0a9b853.svg"
+  },
+  {
+    "revision": "446f4efc2303ff3753469b4cde4aaf41",
+    "url": "/BCCSurvey/static/media/bg_pic_like.446f4efc.svg"
+  },
+  {
+    "revision": "19df80af3e76754d075b58c02d29691a",
+    "url": "/BCCSurvey/static/media/bg_pic_unlike.19df80af.svg"
+  },
+  {
+    "revision": "96e6da5b1cef0b6745c08ef72658993c",
+    "url": "/BCCSurvey/static/media/pic_2_indroduction.96e6da5b.svg"
+  },
+  {
+    "revision": "a39c45ed1594403d78b9735ee0208bc9",
+    "url": "/BCCSurvey/index.html"
+  }
+];
